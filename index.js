ff5ff93ddbc2996c43f6a018509e7127b2978638
jmoseka <jamylam3@gmail.com>
2023-02-23 22:26:04 +0300

    Style our product card

---
+++ index.js
@@ -0,0 +1,3 @@
+import ourProductsData from './modules/our-product-data.js';
+
+console.log(ourProductsData);
---
+++ modules/our-product-data.js
@@ -0,0 +1,35 @@
+const ourProductsData = [
+  {
+    id: 1,
+    category: 'Guitar',
+    title: 'Gaha C20',
+    price: '18000',
+    images: './images/product-images/guitar.png',
+  },
+
+  {
+    id: 2,
+    category: 'Drum',
+    title: 'Tone 3pcs',
+    price: '20000',
+    images: './images/product-images/drums.png',
+  },
+
+  {
+    id: 3,
+    category: 'Orchestral',
+    title: 'Vovo Violin',
+    price: '15000',
+    images: './images/product-images/violin.png',
+  },
+
+  {
+    id: 4,
+    category: 'Piano',
+    title: 'Aros GDP Piano',
+    price: '70000',
+    images: './images/product-images/piano.png',
+  },
+];
+
+export default ourProductsData;
